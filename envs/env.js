e5efe4fc0301a16f9ab60ece1911b3d54332f8a1
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2023-03-14 22:13:00 -0700

    Initial commit

---
+++ envs/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+    '@example_live': 'https://main--example--examplecom.live',
+    '@local3000': 'http://localhost:3000',
+    
+  };
---
+++ features/feature_1.spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'Feature_name',
+    features: [
+      {
+        name: '@feature_name',
+        path: '/test/features/feature_page_name',
+        envs: '@example_live',
+        tags: '@feature_name',
+      },
+    ],
+  };
+  
